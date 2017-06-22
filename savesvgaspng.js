7c26443fa9262406baef54936dc4273dbe185e8b
salshared <sahil_mast@hotmail.com>
2017-06-22 14:30:07 -0400

    Create savesvgaspng.js

---
+++ savesvgaspng.js
@@ -0,0 +1,327 @@
+[
+            {
+                "_id": {
+                    "Debts": {
+                        "DebtDetailId": 3718,
+                        "ProfileEntityId": 313,
+                        "CurrencyId": 178,
+                        "Currency": "USD",
+                        "CurrencyName": "US Dollar",
+                        "Callable": "NULL",
+                        "DebtRank": 1,
+                        "Coupon": "NULL",
+                        "MaturityDate": {
+                            "$date": 1619064000000
+                        },
+                        "DebtDescription": "REVOLVING CREDIT FACILITY",
+                        "ISIN": "NULL",
+                        "CUSIP": "NULL",
+                        "MoodysDebtId": "NULL",
+                        "AmountOutstanding": 0,
+                        "AmountOutstandingInProfileCurrency": 0,
+                        "TotalBankDebtDrawn": 400,
+                        "TotalBankDebtDrawnInProfileCurrency": 400,
+                        "Secure": true,
+                        "DescriptionOfSecurity": "Secured by substantially all assets of the borrowing parties, including (i) pledges of 100% of the stock or other equity interest of each domestic subsidiary that is directly owned by such entity and (ii) 65% of the stock or other equity interest of each foreign subsidiary that is directly owned by such entity.",
+                        "DocUrl": "NULL",
+                        "MICCovenantUrl": "NULL",
+                        "ExplanatoryNotes": "NULL",
+                        "AmountIssued": 400,
+                        "AmountIssuedInProfileCurrency": 400,
+                        "Seniority": "NULL",
+                        "PriorityClaim": "Senior Secured",
+                        "Amortizable": false,
+                        "Year_1": "NULL",
+                        "Year_1InProfileCurrency": "NULL",
+                        "Year_2": "NULL",
+                        "Year_2InProfileCurrency": "NULL",
+                        "Year_3": "NULL",
+                        "Year_3InProfileCurrency": "NULL",
+                        "Year_4": "NULL",
+                        "Year_4InProfileCurrency": "NULL",
+                        "Year_5": "NULL",
+                        "Year_5InProfileCurrency": "NULL",
+                        "Year_6": 0,
+                        "Year_6InProfileCurrency": 0,
+                        "Year_7": "NULL",
+                        "Year_7InProfileCurrency": "NULL",
+                        "Year_8": "NULL",
+                        "Year_8InProfileCurrency": "NULL",
+                        "Year_9": "NULL",
+                        "Year_9InProfileCurrency": "NULL",
+                        "Year_10": "NULL",
+                        "Year_10InProfileCurrency": "NULL",
+                        "AmortizeType": "f",
+                        "VendorSource": false,
+                        "FloatingRate": true,
+                        "LGD": "NULL",
+                        "Guarantees": [
+                            {
+                                "Id": 2040,
+                                "DebtDetailId": 3718,
+                                "GuarantorEntity": {
+                                    "EntityId": 4679,
+                                    "EntityName": "Guarantor Subsidiaries - Central Garden & Pet Company",
+                                    "EntityActive": true,
+                                    "TypeId": 19,
+                                    "EntityTypeName": "Guarantor",
+                                    "MoodysOrgId": "NULL",
+                                    "Ticker": "NULL",
+                                    "CountryId": "NULL",
+                                    "Country": "NULL",
+                                    "CountryName": "NULL"
+                                },
+                                "AmountGuaranteed": 100,
+                                "AmountGuaranteedInProfileCurrency": 100,
+                                "GuarantorType": "NULL"
+                            }
+                        ],
+                        "CallSchedules": "NULL",
+                        "Documents": [
+                            {
+                                "Id": 3010,
+                                "DebtDetailId": 3718,
+                                "DocDate": {
+                                    "$date": 1461643200000
+                                },
+                                "DocType": "8-K",
+                                "DocUrl": "https://www.sec.gov/Archives/edgar/data/887733/000119312516556455/d176137d8k.htm"
+                            },
+                            {
+                                "Id": 3011,
+                                "DebtDetailId": 3718,
+                                "DocDate": {
+                                    "$date": 1462420800000
+                                },
+                                "DocType": "Other",
+                                "DocUrl": "https://www.sec.gov/Archives/edgar/data/887733/000162828016015672/exhibit101suntrust_central.htm"
+                            }
+                        ],
+                        "Comments": [
+                            {
+                                "Id": 309,
+                                "DebtDetailId": 3718,
+                                "UserId": 38,
+                                "Type": "DebtDetails",
+                                "CommentText": "There are multiple borrowers under the revolving credit facility\t\t\t\t\t\t\t\n",
+                                "Publish": true
+                            }
+                        ]
+                    },
+                    "DateForSorting": {
+                        "$date": 1619064000000
+                    }
+                }
+            },
+            {
+                "_id": {
+                    "Debts": {
+                        "DebtDetailId": 3719,
+                        "ProfileEntityId": 313,
+                        "CurrencyId": 178,
+                        "Currency": "USD",
+                        "CurrencyName": "US Dollar",
+                        "Callable": "NULL",
+                        "DebtRank": 2,
+                        "Coupon": 6.125,
+                        "MaturityDate": {
+                            "$date": 1700024400000
+                        },
+                        "DebtDescription": "SR NOTES",
+                        "ISIN": "NULL",
+                        "CUSIP": "NULL",
+                        "MoodysDebtId": "NULL",
+                        "AmountOutstanding": 400,
+                        "AmountOutstandingInProfileCurrency": 400,
+                        "TotalBankDebtDrawn": 0,
+                        "TotalBankDebtDrawnInProfileCurrency": 0,
+                        "Secure": false,
+                        "DescriptionOfSecurity": "NULL",
+                        "DocUrl": "NULL",
+                        "MICCovenantUrl": "NULL",
+                        "ExplanatoryNotes": "NULL",
+                        "AmountIssued": 400,
+                        "AmountIssuedInProfileCurrency": 400,
+                        "Seniority": "NULL",
+                        "PriorityClaim": "Senior Unsecured",
+                        "Amortizable": false,
+                        "Year_1": "NULL",
+                        "Year_1InProfileCurrency": "NULL",
+                        "Year_2": "NULL",
+                        "Year_2InProfileCurrency": "NULL",
+                        "Year_3": "NULL",
+                        "Year_3InProfileCurrency": "NULL",
+                        "Year_4": "NULL",
+                        "Year_4InProfileCurrency": "NULL",
+                        "Year_5": "NULL",
+                        "Year_5InProfileCurrency": "NULL",
+                        "Year_6": "NULL",
+                        "Year_6InProfileCurrency": "NULL",
+                        "Year_7": "NULL",
+                        "Year_7InProfileCurrency": "NULL",
+                        "Year_8": 400,
+                        "Year_8InProfileCurrency": 400,
+                        "Year_9": "NULL",
+                        "Year_9InProfileCurrency": "NULL",
+                        "Year_10": "NULL",
+                        "Year_10InProfileCurrency": "NULL",
+                        "AmortizeType": "f",
+                        "VendorSource": false,
+                        "FloatingRate": false,
+                        "LGD": "NULL",
+                        "Guarantees": [
+                            {
+                                "Id": 2039,
+                                "DebtDetailId": 3719,
+                                "GuarantorEntity": {
+                                    "EntityId": 4679,
+                                    "EntityName": "Guarantor Subsidiaries - Central Garden & Pet Company",
+                                    "EntityActive": true,
+                                    "TypeId": 19,
+                                    "EntityTypeName": "Guarantor",
+                                    "MoodysOrgId": "NULL",
+                                    "Ticker": "NULL",
+                                    "CountryId": "NULL",
+                                    "Country": "NULL",
+                                    "CountryName": "NULL"
+                                },
+                                "AmountGuaranteed": 100,
+                                "AmountGuaranteedInProfileCurrency": 100,
+                                "GuarantorType": "NULL"
+                            }
+                        ],
+                        "CallSchedules": [
+                            {
+                                "Id": 1086,
+                                "DebtDetailId": 3719,
+                                "Date": {
+                                    "$date": 1636952400000
+                                },
+                                "Price": 100
+                            },
+                            {
+                                "Id": 1087,
+                                "DebtDetailId": 3719,
+                                "Date": {
+                                    "$date": 1605416400000
+                                },
+                                "Price": 101.531
+                            },
+                            {
+                                "Id": 1088,
+                                "DebtDetailId": 3719,
+                                "Date": {
+                                    "$date": 1573794000000
+                                },
+                                "Price": 103.063
+                            },
+                            {
+                                "Id": 1089,
+                                "DebtDetailId": 3719,
+                                "Date": {
+                                    "$date": 1542258000000
+                                },
+                                "Price": 104.594
+                            }
+                        ],
+                        "Documents": [
+                            {
+                                "Id": 3007,
+                                "DebtDetailId": 3719,
+                                "DocDate": {
+                                    "$date": 1268024400000
+                                },
+                                "DocType": "Bond Offering Document",
+                                "DocUrl": "https://www.sec.gov/Archives/edgar/data/887733/000119312510049627/dex42.htm"
+                            },
+                            {
+                                "Id": 3008,
+                                "DebtDetailId": 3719,
+                                "DocDate": {
+                                    "$date": 1449723600000
+                                },
+                                "DocType": "Other",
+                                "DocUrl": "https://www.sec.gov/Archives/edgar/data/887733/000119312515399914/d78823dex46.htm"
+                            },
+                            {
+                                "Id": 3009,
+                                "DebtDetailId": 3719,
+                                "DocDate": {
+                                    "$date": 1446699600000
+                                },
+                                "DocType": "Bond Offering Document",
+                                "DocUrl": "https://www.sec.gov/Archives/edgar/data/887733/000119312515367041/d55906d424b2.htm"
+                            }
+                        ],
+                        "Comments": "NULL"
+                    },
+                    "DateForSorting": {
+                        "$date": 1700024400000
+                    }
+                }
+            },
+            {
+                "_id": {
+                    "Debts": {
+                        "DebtDetailId": 3717,
+                        "ProfileEntityId": 313,
+                        "CurrencyId": 178,
+                        "Currency": "USD",
+                        "CurrencyName": "US Dollar",
+                        "Callable": "NULL",
+                        "DebtRank": 99,
+                        "Coupon": "NULL",
+                        "MaturityDate": "NULL",
+                        "DebtDescription": "OTHER DEBT- Other Notes Payable",
+                        "ISIN": "NULL",
+                        "CUSIP": "NULL",
+                        "MoodysDebtId": "NULL",
+                        "AmountOutstanding": 0.967,
+                        "AmountOutstandingInProfileCurrency": 0.967,
+                        "TotalBankDebtDrawn": 0,
+                        "TotalBankDebtDrawnInProfileCurrency": 0,
+                        "Secure": true,
+                        "DescriptionOfSecurity": "TO BE REVIEWED - ASSUMED SECURED",
+                        "DocUrl": "NULL",
+                        "MICCovenantUrl": "NULL",
+                        "ExplanatoryNotes": "NULL",
+                        "AmountIssued": "NULL",
+                        "AmountIssuedInProfileCurrency": "NULL",
+                        "Seniority": "NULL",
+                        "PriorityClaim": "Other",
+                        "Amortizable": false,
+                        "Year_1": 0.0967,
+                        "Year_1InProfileCurrency": 0.0967,
+                        "Year_2": 0.1934,
+                        "Year_2InProfileCurrency": 0.1934,
+                        "Year_3": 0.1934,
+                        "Year_3InProfileCurrency": 0.1934,
+                        "Year_4": 0.1934,
+                        "Year_4InProfileCurrency": 0.1934,
+                        "Year_5": 0.1934,
+                        "Year_5InProfileCurrency": 0.1934,
+                        "Year_6": 0.0967,
+                        "Year_6InProfileCurrency": 0.0967,
+                        "Year_7": "NULL",
+                        "Year_7InProfileCurrency": "NULL",
+                        "Year_8": "NULL",
+                        "Year_8InProfileCurrency": "NULL",
+                        "Year_9": "NULL",
+                        "Year_9InProfileCurrency": "NULL",
+                        "Year_10": "NULL",
+                        "Year_10InProfileCurrency": "NULL",
+                        "AmortizeType": "f",
+                        "VendorSource": false,
+                        "FloatingRate": "NULL",
+                        "LGD": "NULL",
+                        "Guarantees": "NULL",
+                        "CallSchedules": "NULL",
+                        "Documents": "NULL",
+                        "Comments": "NULL"
+                    },
+                    "DateForSorting": {
+                        "$date": 221845392000000
+                    }
+                }
+            }
+        
